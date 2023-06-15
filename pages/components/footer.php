bc073d13d32089df94ae4d2efcda71df0b86a796
LucasBilbao <lucasbm789@gmail.com>
2023-06-15 20:15:00 +0400

    feat: add create-test-page

---
+++ pages/components/footer.php
@@ -0,0 +1,24 @@
+<footer>
+  <ul class="center">
+    <li class="primary">
+      <a href="../index.html">
+        <img src="../images/logo.png" alt="Luka's Quizzes" />
+      </a>
+    </li>
+    <li>
+      <a href="tel:+995558555390" data-contact data-phone data-info="+995558555390">phone</a>
+    </li>
+    <li>
+      <a href="mailto:lucasbm789@gmail.com" data-contact data-email data-info="lucasbm789@gmail.com">email</a>
+    </li>
+    <li>
+      <a href="https://www.facebook.com/Lu.Kas10002/" target="blank" data-contact data-facebook>facebook</a>
+    </li>
+  </ul>
+  <div class="bottom-border"></div>
+  <div class="overlay inactive opaque"></div>
+  <div data-spinner class="spinner inactive"></div>
+  <div class="copy-message" data-copied-hint style="opacity: 0">
+    <p>Copied to clipboard!</p>
+  </div>
+</footer>
---
+++ pages/components/header.php
@@ -0,0 +1,25 @@
+<header>
+  <div class="center">
+    <h1>
+      <a href="../index.html"><img src="../images/logo.png" alt="Luka's Quizzes" /></a>
+      <a href="../index.html">luka's Quizzes</a>
+    </h1>
+    <div class="authorization">
+      <button data-profile-btn class="">profile</button>
+      <div class="dropdown_menu">
+        <ul>
+          <li><a href="./profile.html">profile</a></li>
+          <li><a href="#">history</a></li>
+          <li><button data-logout-btn>logout</button></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <dialog data-logout-dialog>
+    <div class="wrapper">
+      <h2>Are you sure you want to log out?</h2>
+      <button data-confirm-logout class="primary_button_link">yes</button>
+      <button data-cancel-logout class="secondary_button_link">no</button>
+    </div>
+  </dialog>
+</header>
